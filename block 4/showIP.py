6a87b47316c1de6f2a06cd01666d0f5fe39d4158
Michiel Bontenbal <37507786+MichielBbal@users.noreply.github.com>
2019-11-13 14:17:51 +0100

    Update showIP.py

--- block 4/showIP.py
+++ block 4/showIP.py
@@ -65,6 +65,7 @@ def main():
   lcd_init()
   lcd_string("if network known", LCD_LINE_1)
   lcd_string("IP address in60s", LCD_LINE_2)
+  time.sleep(60)
   while True:
     myIP = str((([ip for ip in socket.gethostbyname_ex(socket.gethostname())[2] if not ip.startswith("127.")] or [[(s.connect(("8.8.8.8", 53)), s.getsockname()[0], s.close()) for s in [socket.socket(socket.AF_INET, socket.SOCK_DGRAM)]][0][1]]) + ["no IP found"])[0])
     lcd_string(myIP,LCD_LINE_1)
