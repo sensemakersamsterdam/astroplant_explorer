8f48f8f025734290aca92a148262fc007ca92fb1
tvdtogt <ted@vandertogt.nl>
2019-11-29 16:59:39 +0100

    added pwm

--- block 4/simple_light.py
+++ block 4/simple_light.py
@@ -1,35 +1,52 @@
-# this is a first concept; code for pwm steering of LEDs still to be added...
-# for explanation of time functions see https://docs.python.org/3/library/time.…
+# this is a first concept; 
 # this script uses time in minutes: every day has 24 x 60 = 1440 minutes 
-# every minute conditions are evaluated and lighting changed when needed
+# a while loop generates a time (with 25 min in between) and sets the light accordingly
 
+import RPi.GPIO as GPIO
 import time
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+GPIO.setup(18, GPIO.OUT) #farred (or green)
+GPIO.setup(20, GPIO.OUT) #red
+GPIO.setup(21, GPIO.OUT) # blue
+pwm_farred = GPIO.PWM(18, 100)  # Initialize PWM on pwmPin 100Hz frequency
+pwm_red = GPIO.PWM(20, 100)  # Initialize PWM on pwmPin 100Hz frequency
+pwm_blue = GPIO.PWM(21, 100)  # Initialize PWM on pwmPin 100Hz frequency
+pwm_farred.start(0)
+pwm_red.start(0)
+pwm_blue.start(0)
 
 def red(t):
     if 0<= t <360:
         return (0)
     elif 360<= t < 1320:
-        return (0.2)
+        return (100)
     else:
         return (0)
 
 def blue(t):
     if 0<= t <360:
         return (0)
-    elif 360<= t < 1320:
-        return (0.1)
+    elif 720<= t < 1320:
+        return (100)
     else:
         return (0)
     
 def farred(t):
     if 0<= t <360:
         return (0)
-    elif 360<= t < 1320:
-        return (0.05)
+    elif 360<= t < 1080:
+        return (100)
     else:
         return (0)
 
-while True:
-    thisminute= (time.localtime()[3] * 60 + time.localtime()[4]) 
-    print("red is set to "+str(red(thisminute))+ ", blue to "+str(blue(thisminute))+ " and farred to "+str(farred(thisminute)))
-    time.sleep(60)
+def setLight(t):
+    pwm_farred.ChangeDutyCycle(farred(t))
+    pwm_red.ChangeDutyCycle(red(t))
+    pwm_blue.ChangeDutyCycle(blue(t))
+    print("On "+str(n//60)+":"+ str(n % 60)+" red is set to "+str(red(t))+ "%, blue to "+str(blue(t))+ "% and farred to "+str(farred(t))+ "%")
+
+for n in range (0, 1439, 25):
+    setLight(n)
+    time.sleep(1)
