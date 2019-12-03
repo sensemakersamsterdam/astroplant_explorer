61b1b09c692b422f2d761ef957c55835e62260a0
Michiel Bontenbal <37507786+MichielBbal@users.noreply.github.com>
2019-12-03 15:58:12 +0100

    Update LED.py

--- block 1/LED.py
+++ block 1/LED.py
@@ -1,10 +1,12 @@
-import RPi.GPIO as GPIO
+import RPi.GPIO as GPIO #import the library to control the GPIO pins
 import time
-GPIO.setmode(GPIO.BCM)
+#settings for GPIO
+GPIO.setmode(GPIO.BCM) 
 GPIO.setwarnings(False)
-GPIO.setup(20, GPIO.OUT)
+GPIO.setup(20, GPIO.OUT)#connect the led to GPIO pin 20
+
 print("LED on")
-GPIO.output(20, GPIO.HIGH)
+GPIO.output(20, GPIO.HIGH) #turn the LED on
 time.sleep(1)
 print("LED off")
-GPIO.output(20, GPIO.LOW)
+GPIO.output(20, GPIO.LOW) #turn the LED off
