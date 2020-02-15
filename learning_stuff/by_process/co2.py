c5f49b4aede4a78c435d3732ae6057c44dfd5be1
tvdtogt <ted@vandertogt.nl>
2020-02-15 14:20:44 +0100

    added mqtt reconnect and logging

--- learning_stuff/by_process/co2.py
+++ learning_stuff/by_process/co2.py
@@ -2,27 +2,28 @@
 # http://qiita.com/UedaTakeyuki/items/c5226960a7328155635f
 import serial
 import time
-import RPi.GPIO as GPIO
 
-GPIO.setmode(GPIO.BCM)
+ser = serial.Serial('/dev/ttyS0',
+                    baudrate=9600,
+                    bytesize=serial.EIGHTBITS,
+                    parity=serial.PARITY_NONE,
+                    stopbits=serial.STOPBITS_ONE,
+                    timeout=1.0)
 
 def mh_z19():
-  ser = serial.Serial('/dev/ttyS0',
-                      baudrate=9600,
-                      bytesize=serial.EIGHTBITS,
-                      parity=serial.PARITY_NONE,
-                      stopbits=serial.STOPBITS_ONE,
-                      timeout=1)
-  while 1:
     command = "\xff\x01\x86\x00\x00\x00\x00\x00\x79"
-    #result=ser.write("\xff\x01\x86\x00\x00\x00\x00\x00\x79")
     result=ser.write(bytes(command, 'latin1'))
-
     s=ser.read(9)
     s = s.decode('latin1')
-    #print(s)
+    #print(s) #for debugging
     if s[0] == "\xff" and s[1] == "\x86":
-      #return {'co2': ord(s[2])*256 + ord(s[3])}
-      return ord(s[2])*256+ord(s[3])
-   
-print(mh_z19())
+        return (ord(s[2])*256 + ord(s[3]))
+    else:
+        return 0
+
+if __name__ == '__main__':
+  while True:
+    value = mh_z19()
+    print ("co2=", value)
+    #readings of this sensor are per 5 seconds
+    time.sleep(5)
