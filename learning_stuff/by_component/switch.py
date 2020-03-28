ced79dcce707b88030b74e248b3d8a0d2ffad41b
tvdtogt <ted@vandertogt.nl>
2020-03-28 15:06:36 +0100

    improved indentation

--- learning_stuff/by_component/switch.py
+++ learning_stuff/by_component/switch.py
@@ -1,10 +1,10 @@
 import RPi.GPIO as GPIO
 import time
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(5, GPIO.IN)
+GPIO.setup(7, GPIO.IN)
 prev_input = 0
 while True:
-    input = GPIO.input(5)
+    input = GPIO.input(7)
     if ((not prev_input) and input):
         print("Button pressed")
     prev_input = input
